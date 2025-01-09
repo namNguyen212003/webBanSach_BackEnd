52dc60980fa7daeea1b0543a95690d3d647999c6
Nguyen Khac Hoai Nam <namman212003@gmail.com>
2025-01-09 15:21:37 +0700

    tao du an moi

---
+++ src/main/java/com/webBanSach_BackEnd/webBanSach_BackEnd/WebBanSachBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.webBanSach_BackEnd.webBanSach_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanSachBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webBanSach_BackEnd/webBanSach_BackEnd/WebBanSachBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webBanSach_BackEnd.webBanSach_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanSachBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
