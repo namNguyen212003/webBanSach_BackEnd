c6f28a6216a8610a8633db7fcd34428700438363
Hoai Nam <namman212003@gmail.com>
2025-01-13 23:02:49 +0700

    tao du an moi

---
+++ My_Home_Documents/webbansach_backend/src/test/java/webbansach_backend/com/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package webbansach_backend.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
